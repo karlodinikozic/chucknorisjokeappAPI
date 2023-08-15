3d1fbb999c22e0c59440fdc7a8a3911f8426e0a5
karlodinikozic <karlo.kozic@serapion.net>
2023-08-15 16:05:04 +0200

    - Added fetching of Chuck Noris joke logic with axios get random joke

--- src/joke/controllers/jokeController.ts
+++ src/joke/controllers/jokeController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import jokeService from "../services/jokeService";
 
-const getJoke = (_: Request, res: Response) => {
-  return res.json(jokeService.getJoke());
+const getJoke = async (_: Request, res: Response) => {
+  return res.json(await jokeService.getJoke());
 };
 
 
--- src/joke/services/jokeService.ts
+++ src/joke/services/jokeService.ts
@@ -1,5 +1,13 @@
-const getJoke = () => {
-  return "This is a joke";
+import axios from "axios";
+
+const URL = process.env.JOKE_API_URL;
+const getJoke = async () => {
+  try {
+    const res = await axios.get(`${URL}/jokes/random`);
+    return res.data;
+  } catch (e) {
+    throw new Error("Error occurred while fetching the app\n Error: " + e);
+  }
 };
 
 export default { getJoke };
